10009f6a5902b234989fac99457ca2aa8f7dbf6e
cdevalero <onvalero@gmail.com>
2020-09-21 21:50:57 -0430

    add viwes admin table

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 
 urlpatterns = [
+    path('', index_admin, name="index_admin"),
+    path('lugar_dir/', lugar_dir_admin, name="lugar_dir_admin"),
+    path('companias/', companias_admin, name="companias_admin"),
+    path('particulares/', particulares_admin, name="particulares_admin"),
+    path('oficinas/', oficinas_admin, name="oficinas_admin"),
+    path('camiones/', camiones_admin, name="camiones_admin"),
+    path('remolques/', remolques_admin, name="remolques_admin"),
+    path('contratos/', contratos_admin, name="contratos_admin"),
 ]
--- administracion/views.py
+++ administracion/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from alquiler.models import *
 
-# Create your views here.
+def index_admin(request):
+    return render(request, 'base_admin.html')
+
+def lugar_dir_admin(request):
+    obj = Lugares_Dir.objects.all()
+    return render(request, 'tablas/lugar_dir.html', {'lugares': obj})
+
+def companias_admin(request):
+    obj = Companias.objects.all()
+    return render(request, 'tablas/companias.html', {'companias': obj})
+
+def particulares_admin(request):
+    obj = Particulares.objects.all()
+    return render(request, 'tablas/particulares.html', {'particulares': obj})
+
+def oficinas_admin(request):
+    obj = Oficinas.objects.all()
+    return render(request, 'tablas/oficinas.html', {'oficinas': obj})
+
+def camiones_admin(request):
+    obj = Camiones.objects.all()
+    return render(request, 'tablas/camiones.html', {'camiones': obj})
+
+def remolques_admin(request):
+    obj = Remolques.objects.all()
+    return render(request, 'tablas/remolques.html', {'remolques': obj})
+
+def contratos_admin(request):
+    obj = Contrartos.objects.all()
+    return render(request, 'tablas/contratos.html', {'contratos': obj})
+
