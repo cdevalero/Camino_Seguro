b1d2c690cf4fed4217b0324b5700cfbf77165891
cdevalero <onvalero@gmail.com>
2020-09-24 20:43:56 -0430

    add pedido

--- alquiler/urls.py
+++ alquiler/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('registro/compania', registro_user_compania, name="registro_user_compania"),
     path('registro/opcion', registro_user_opcion, name="registro_user_opcion"),
     path('', inicio_user, name="inicio_user"),
+    path('pedido/', pedido, name="pedido"),
 ]
--- alquiler/views.py
+++ alquiler/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import *
 from django.http import HttpResponse
 from django.contrib import messages
+from datetime import timedelta
 
 def registro_user_persona(request):
     if request.method == 'POST':
@@ -31,6 +32,7 @@ def registro_user_compania(request):
     return render(request, 'register.html', {'form':form})                
 
 def inicio_user(request):
+    
     if request.method == 'POST':
         form = FormParticulares_inicio(request.POST)
         if form.is_valid():
@@ -43,12 +45,54 @@ def inicio_user(request):
                 except Companias.DoesNotExist:
                     messages.error(request, 'No existe el usuario, por favor registrarse')
                     return redirect('inicio_user')            
-            return redirect('particulares_admin')
+            return redirect('pedido')
         messages.error(request, 'No existe el usuario, por favor registrarse (invalido)')
         return redirect('inicio_user')
     form = FormParticulares_inicio()
     return render(request, 'inicio.html', {'form':form})
 
-def pedido(request, cliente):
-    
-    pass
+def pedido(request):
+    if request.method == 'POST':
+        form = FormContratos(request.POST)
+        if form.is_valid():
+            if form.cleaned_data.get('id_camion') and form.cleaned_data.get('id_remolque'):
+                messages.error(request, 'No se puede conectar un contrato con un Camion y un Remolque a la vez')
+                return render(request, 'pedido.html',{'form':form})
+            if form.cleaned_data.get('id_per') and form.cleaned_data.get('id_compa'):
+                messages.error(request, 'No se puede conectar un contrato con una Persona y una Compañia a la vez')
+                return render(request, 'pedido.html',{'form':form})
+            if form.cleaned_data.get('id_per'):
+                persona = form.cleaned_data.get('id_per')
+                if persona.riesgo:
+                    dni = persona.dni
+                    messages.error(request, f'El cliente DNI:{dni} es una persona con estado de riesgo')
+                    return redirect('pedido')
+            if form.cleaned_data.get('id_camion'):
+                camion = form.cleaned_data.get('id_camion')
+                camion = Contrartos.objects.filter(id_camion=camion.id)
+                fecha = form.cleaned_data.get('fecha_alquiler')
+                if camion:
+                    for cam in camion:
+                        fecha_cam = cam.fecha_alquiler
+                        fecha_cam = fecha_cam + timedelta(days=cam.dura)
+                        if fecha_cam > fecha and fecha > cam.fecha_alquiler:
+                            messages.error(request, 'El Camion esta en uso para esa fecha de alquiler')
+                            return redirect('pedido')
+            if form.cleaned_data.get('id_remolque'):
+                remolque = form.cleaned_data.get('id_remolque')
+                remolque = Contrartos.objects.filter(id_remolque=remolque.id)
+                fecha = form.cleaned_data.get('fecha_alquiler')
+                if remolque:
+                    for remo in remolque:
+                        fecha_remo = remo.fecha_alquiler
+                        fecha_remo = fecha_remo + timedelta(days=remo.dura)
+                        if fecha_remo > fecha and fecha > remo.fecha_alquiler:
+                            messages.error(request, 'El Remolque esta en uso para esa fecha de alquiler')
+                            return redirect('pedido')
+            form.save()
+            return redirect('pedido')
+        else:
+            messages.error(request, 'Form invalido')
+            return redirect('pedido')
+    form = FormContratos()
+    return render(request, 'pedido.html',{'form':form})
